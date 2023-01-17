d6d113d4e9548cbe104b0804204c3c01c58d9945
Emiliya Kirina <emiliya.kirina@gmail.com>
2023-01-17 10:24:14 +0200

    Upload excersizes

---
+++ Programming Basics with Python/01. First-Steps-in-Coding/01.PB-Python-First-Steps-in-Coding-Lab/pet_shop.py
@@ -0,0 +1,5 @@
+dog_food_num = int(input())
+cat_food_num = int(input())
+
+total_sum = dog_food_num * 2.50 + cat_food_num * 4.00
+print (f"{total_sum} lv.")
---
+++ Programming Basics with Python/01. First-Steps-in-Coding/01.PB-Python-First-Steps-in-Coding-Lab/projects_creation.py
@@ -0,0 +1,5 @@
+time_per_project = 3
+architects_name = input()
+number_of_projects = int(input())
+total_time = time_per_project * number_of_projects
+print (f"The architect {architects_name} will need {total_time} hours to complete {number_of_projects} project/s.")
