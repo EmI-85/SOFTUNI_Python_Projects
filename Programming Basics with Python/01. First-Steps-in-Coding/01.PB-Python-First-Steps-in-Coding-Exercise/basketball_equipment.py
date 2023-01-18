5c7497ab8c8a37f201ec4807e3f175d34a172a0e
Emiliya Kirina <emiliya.kirina@gmail.com>
2023-01-18 10:59:54 +0200

    Create basketball equipment

---
+++ Programming Basics with Python/01. First-Steps-in-Coding/01.PB-Python-First-Steps-in-Coding-Exercise/basketball_equipment.py
@@ -0,0 +1,10 @@
+annual_fee = int(input())
+
+shoes = annual_fee * 0.60
+track_suit = shoes * 0.80
+ball = track_suit * 0.25
+accessories = ball * 0.20
+
+total_sum = annual_fee + shoes + track_suit + ball + accessories
+print(total_sum)
+
