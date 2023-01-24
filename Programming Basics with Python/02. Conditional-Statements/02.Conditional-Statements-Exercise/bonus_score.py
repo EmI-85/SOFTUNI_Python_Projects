7768cf1c1283aca9e2a9e921c9291c87d1cd908d
Emiliya Kirina <emiliya.kirina@gmail.com>
2023-01-24 13:00:33 +0200

    Create bonus_score.py

---
+++ Programming Basics with Python/02. Conditional-Statements/02.Conditional-Statements-Exercise/bonus_score.py
@@ -0,0 +1,19 @@
+num = int(input())
+bonus = 0
+
+if num <= 100:
+    bonus = 5
+elif num <= 1000:
+    bonus = num * 0.20
+else:
+    bonus = num * 0.10
+
+if num % 2 == 0:
+    bonus = bonus + 1
+elif num % 10 == 5:
+    bonus = bonus + 2
+
+total_points = num + bonus
+
+print(bonus)
+print(total_points)
