044f5600793e4f0497adf6bffffc8a66df4684e9
Emiliya Kirina <emiliya.kirina@gmail.com>
2023-01-29 15:42:49 +0200

    Create sum_numbers.py

---
+++ Programming Basics with Python/04. For-Loop/04.PB-Python-For-Loop-Lab/sum_numbers.py
@@ -0,0 +1,7 @@
+numbers_count = int(input())
+sum_of_numbers = 0
+
+for numbers in range(numbers_count):
+    current_number = int(input())
+    sum_of_numbers += current_number
+print(sum_of_numbers)
