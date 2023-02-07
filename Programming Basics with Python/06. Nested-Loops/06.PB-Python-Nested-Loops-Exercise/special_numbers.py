3ce5f2fd10d3c96dbdc5da0181a08cc37a56ac49
Emiliya Kirina <emiliya.kirina@gmail.com>
2023-02-07 15:32:42 +0200

    Create special_numbers.py

---
+++ Programming Basics with Python/06. Nested-Loops/06.PB-Python-Nested-Loops-Exercise/special_numbers.py
@@ -0,0 +1,10 @@
+number = int(input())
+
+for i in range(1, 9 + 1):
+    for j in range(1, 9 + 1):
+        for k in range(1, 9 + 1):
+            for l in range(1, 9 + 1):
+                is_valid = number % i == 0 and number % j == 0 and number % k == 0 and number % l == 0
+                if is_valid:
+                    print(f"{i}{j}{k}{l}", end = " ")
+
