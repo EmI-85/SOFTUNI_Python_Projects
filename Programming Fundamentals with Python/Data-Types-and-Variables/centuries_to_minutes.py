9c58a69f7d327f56f92cc3330c89d653f60b4832
Emiliya Kirina <emiliya.kirina@gmail.com>
2023-03-17 17:37:53 +0200

    Create centuries_to_minutes.py

---
+++ Programming Fundamentals with Python/Data-Types-and-Variables/centuries_to_minutes.py
@@ -0,0 +1,9 @@
+import math
+
+centuries = int(input())
+years = math.floor(centuries * 100)
+days = math.floor(years * 365.2422)
+hours = math.floor(days * 24)
+minutes = math.floor(hours * 60)
+print(f"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes")
+
